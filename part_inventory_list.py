e0eec0b38d061ed7f2c0a143a6a699d7e48168d9
vmendi <vmendi@gmail.com>
2016-05-27 22:24:03 -0700

    Better right list

--- part_inventory_list.py
+++ part_inventory_list.py
@@ -13,7 +13,7 @@ class PartInventoryList (Frame):
         self.hbar.pack(side=BOTTOM, fill=X)
         self.hbar.config(command=self.canvas.xview)
 
-        self.vbar=Scrollbar(self, orient=VERTICAL)
+        self.vbar = Scrollbar(self, orient=VERTICAL)
         self.vbar.pack(side=RIGHT, fill=Y)
         self.vbar.config(command=self.canvas.yview)
 
@@ -21,19 +21,84 @@ class PartInventoryList (Frame):
         self.canvas.pack(side=LEFT, expand=True, fill=BOTH)
 
         self.inner_frame = Frame(self.canvas)
-        self.inner_frame.pack(expand=True, fill=BOTH)
+        self.inner_frame.grid()
+        self.inner_frame.grid_columnconfigure(1, weight=1)
+        self.inner_frame.bind("<Configure>", self.on_inner_frame_configure)
 
         self.canvas_window = self.canvas.create_window((0, 0), anchor='nw', window=self.inner_frame,
                                                        tags="self.inner_frame")
 
-        self.inner_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        signal('on_mouse_click_part').connect(self.on_mouse_click_part)
 
-        signal('on_mouse_click_part').connect(self.add_part)
+        self.part_model = PartListModel()
 
-    def add_part(self, sender, part):
+
+    def on_inner_frame_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        self.canvas.itemconfig(self.canvas_window, width=self.canvas.winfo_width() - self.vbar.winfo_width())
+
+
+    def on_mouse_click_part(self, sender, part):
+        part_entry = self.part_model.add_part_entry(part, None)
+
+        if part_entry['count'] == 1:
+            self.create_part_entry(part_entry)
+        else:
+            self.update_part_entry(part_entry)
+
+
+    def create_part_entry(self, part_entry):
         grid_size = self.inner_frame.grid_size()
+
         next_row_index = grid_size[1] + 1
+
+        part = part_entry['part']
+        part_count = part_entry['count']
+
         new_image = fetch_image.create_image_label(part, self.inner_frame, 0.5)
-        new_image.grid(row = next_row_index, column=0, sticky='w')
-        new_label = Label(self.inner_frame, text=part['number'])
-        new_label.grid(row = next_row_index, column=1, sticky='nsw')
+        new_image.grid(row=next_row_index, column=0, sticky='w', padx=5)
+        new_number = Label(self.inner_frame, text=part['number'])
+        new_number.grid(row=next_row_index, column=1, sticky='w', padx=5)
+        new_count = Label(self.inner_frame, text=part_count)
+        new_count.grid(row=next_row_index, column=2)
+
+
+    def update_part_entry(self, part_entry):
+        grid_size = self.inner_frame.grid_size()
+
+        for row_index in range(grid_size[1]):
+            ui_entry = self.inner_frame.grid_slaves(row_index)
+            if len(ui_entry) > 0 and ui_entry[1]['text'] == part_entry['part']['number']:
+                ui_entry[0]['text'] = part_entry['count']
+                break
+
+
+class PartListModel:
+    def __init__(self):
+        self.part_entries = []
+
+    def add_part_entry(self, part, color):
+        part_entry = self.find_part_entry(part)
+
+        if part_entry:
+            part_entry['count'] += 1
+        else:
+            part_entry = dict(part=part,
+                              count=1,
+                              color="blah")
+
+            self.part_entries.append(part_entry)
+
+        return part_entry
+
+    def find_part_entry(self, part):
+        for part_entry in self.part_entries:
+            if part['number'] == part_entry['part']['number']:
+                return part_entry
+        return None
+
+    def contains_part(self, part):
+        for part_entry in self.part_entries:
+            if part['number'] == part_entry['part']['number']:
+                return True
+        return False
